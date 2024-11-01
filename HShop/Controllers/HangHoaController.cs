b907cb5a44bda5b1a5508bc5087d048238084fb2
Blink271 <daotuanvu27012k3@gmail.com>
2024-11-01 23:14:32 +0700

    "danhsachsp"

--- HShop/Controllers/HangHoaController.cs
+++ HShop/Controllers/HangHoaController.cs
@@ -1,12 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HShop.Data;
+using HShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HShop.Controllers
 {
     public class HangHoaController : Controller
     {
+        private readonly Hshop2023Context db;
+        public HangHoaController(Hshop2023Context context)
+        {
+            db = context;
+        }
         public IActionResult Index(int? loai)
         {
-            return View();
+            var hanghoas = db.HangHoas.AsQueryable();
+            if (loai.HasValue)
+            {
+                hanghoas = hanghoas.Where(p => p.MaLoai == loai.Value);
+            }
+
+            var result = hanghoas.Select(p => new HangHoaVM
+            {
+                MaHH = p.MaHh,
+                TenHH = p.TenHh,
+                Hinh = p.Hinh ?? "",
+                DonGia = p.DonGia ?? 0,
+                MotaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+             
+            return View(result);
+        }
+
+        public IActionResult Search(string? query)
+        {
+            var hanghoas = db.HangHoas.AsQueryable();
+            if (query != null)
+            {
+                hanghoas = hanghoas.Where(p => p.TenHh.Contains(query));
+            }
+
+            var result = hanghoas.Select(p => new HangHoaVM
+            {
+                MaHH = p.MaHh,
+                TenHH = p.TenHh,
+                Hinh = p.Hinh ?? "",
+                DonGia = p.DonGia ?? 0,
+                MotaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+
+            return View(result);
         }
     }
 }
---
+++ HShop/ViewModels/HangHoaVM.cs
@@ -0,0 +1,13 @@
+﻿namespace HShop.ViewModels
+{
+    public class HangHoaVM
+    {
+        public int MaHH { get; set; }
+        public string TenHH { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+        public string MotaNgan { get; set; }
+        public string TenLoai { get; set; }
+
+    }
+}
