1f9dad2608f02c05a8f03555e4c18fa517678217
Blink271 <daotuanvu27012k3@gmail.com>
2024-11-02 10:43:12 +0700

    "ChiTietHangHoa"

--- HShop/Controllers/HangHoaController.cs
+++ HShop/Controllers/HangHoaController.cs
@@ -1,6 +1,7 @@
 ﻿using HShop.Data;
 using HShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HShop.Controllers
 {
@@ -50,6 +51,31 @@ namespace HShop.Controllers
                 TenLoai = p.MaLoaiNavigation.TenLoai
             });
 
+            return View(result);
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            var data = db.HangHoas
+                .Include(p => p.MaLoaiNavigation)
+                .SingleOrDefault(p => p.MaHh == id);
+            if(data == null)
+            {
+                TempData["Message"] = $"Không tìn thấy sản phẩm có mã {id}";
+                return Redirect("/404");
+            }
+            var result = new ChiTietHangHoaVM
+            {
+                MaHH = data.MaHh,
+                TenHH = data.TenHh,
+                DonGia = data.DonGia ?? 0,
+                ChiTiet = data.MoTa ?? "",
+                Hinh = data.Hinh ?? "",
+                MotaNgan = data.MoTaDonVi ?? "",
+                TenLoai = data.MaLoaiNavigation.TenLoai,
+                SoLuongTon = 10,
+                DiemDanhGia = 5
+            };
             return View(result);
         }
     }
---
+++ HShop/ViewModels/ChiTietHangHoaVM.cs
@@ -0,0 +1,15 @@
+﻿namespace HShop.ViewModels
+{
+    public class ChiTietHangHoaVM
+    {
+        public int MaHH { get; set; }
+        public string TenHH { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+        public string MotaNgan { get; set; }
+        public string TenLoai { get; set; }
+        public string ChiTiet {  get; set; }
+        public int SoLuongTon { get; set; }
+        public int DiemDanhGia { get; set; }
+    }
+}
